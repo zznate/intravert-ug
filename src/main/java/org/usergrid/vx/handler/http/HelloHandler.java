e7ddea7ac64d4545979da32821c294b2b85422fc
Edward Capriolo <edlinuxguru@gmail.com>
2012-11-22 12:36:51 -0500

    Removed UG bits

---
+++ src/main/java/org/usergrid/vx/handler/http/HelloHandler.java
@@ -0,0 +1,14 @@
+package org.usergrid.vx.handler.http;
+
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.http.HttpServerRequest;
+
+public class HelloHandler implements Handler<HttpServerRequest>{
+	
+	@Override
+	public void handle(HttpServerRequest request) {
+        System.out.println("Request handled");
+        request.response.end("ok");
+	}
+	
+}
--- src/main/java/org/usergrid/vx/server/IntravertServer.java
+++ src/main/java/org/usergrid/vx/server/IntravertServer.java
@@ -6,19 +6,16 @@ import org.vertx.java.deploy.Verticle;
 public class IntravertServer {
 
 	public static void main(String[] args) {
-		
 			System.setProperty("cassandra-foreground", "true");
 			System.setProperty("log4j.defaultInitOverride","true");
 			System.setProperty("log4j.configuration", "log4j.properties");
 			AbstractCassandraDaemon.initLog4j();
-			
 			IntravertServer is = new IntravertServer();
 			is.startServer();
-		
 	}
 	
 	private void startServer() {
-		UsergridCassandraDaemon ucd = new UsergridCassandraDaemon();
+		IntravertDaemon ucd = new IntravertDaemon();
 		ucd.activate();
 	}
 
