c0e5bfea3646a3844a861c378751e58579eec815
Edward Capriolo <edlinuxguru@gmail.com>
2013-02-26 23:17:26 -0500

    Make these piece reusable for testing

--- src/main/java/org/usergrid/vx/server/IntravertCassandraServer.java
+++ src/main/java/org/usergrid/vx/server/IntravertCassandraServer.java
@@ -12,7 +12,7 @@
  *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
-*/
+ */
 package org.usergrid.vx.server;
 
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -40,60 +40,55 @@ import org.vertx.java.core.Vertx;
 import org.vertx.java.core.http.RouteMatcher;
 
 public class IntravertCassandraServer implements CassandraDaemon.Server {
-	
-	private final Logger logger = LoggerFactory.getLogger(IntravertCassandraServer.class);
-	private static Vertx vertx;
-	private static RouteMatcher rm;
+
+  private final Logger logger = LoggerFactory.getLogger(IntravertCassandraServer.class);
+  private static Vertx vertx;
+  private static RouteMatcher rm;
   private static IntravertClusterNotifier intravertClusterNotifier;
   private static final AtomicBoolean running = new AtomicBoolean(false);
-	
-	@Override
-	public void start() {
+
+  @Override
+  public void start() {
     logger.debug("Starting IntravertCassandraServer...");
-    // TODO may be more appropriate in setup() ?
-		vertx = Vertx.newVertx();
-		
-		rm = new RouteMatcher();
-		rm.put("/:appid/hello", new HelloHandler());
-		rm.get("/:appid/hello", new HelloHandler());
-		rm.post("/:appid/hello", new HelloHandler());
-		rm.post("/:appid/intrareq-json", new IntraHandlerJson(vertx));
-		rm.post("/:appid/intrareq-jsonsmile", new IntraHandlerJsonSmile(vertx));
-		rm.noMatch(new NoMatchHandler());
-
-            registerOperationHandlers();
-
-		vertx.createHttpServer().requestHandler(rm).listen(8080);
-		logger.info("IntravertCassandraServer started.");
+    vertx = Vertx.newVertx();
+    rm = new RouteMatcher();
+    rm.put("/:appid/hello", new HelloHandler());
+    rm.get("/:appid/hello", new HelloHandler());
+    rm.post("/:appid/hello", new HelloHandler());
+    rm.post("/:appid/intrareq-json", new IntraHandlerJson(vertx));
+    rm.post("/:appid/intrareq-jsonsmile", new IntraHandlerJsonSmile(vertx));
+    rm.noMatch(new NoMatchHandler());
+    registerOperationHandlers(vertx);
+    vertx.createHttpServer().requestHandler(rm).listen(8080);
+    logger.info("IntravertCassandraServer started.");
     running.set(true);
     intravertClusterNotifier = IntravertClusterNotifier.forServer(vertx);
-	}
+  }
 
-	@Override
-	public void stop() {
-    boolean stopped = running.compareAndSet(true,false);
-		logger.info("stopServer...{}", stopped);
-
-	}
+  @Override
+  public void stop() {
+    boolean stopped = running.compareAndSet(true, false);
+    logger.info("stopServer...{}", stopped);
+  }
 
   @Override
   public boolean isRunning() {
     return running.get();
   }
 
-    private void registerOperationHandlers() {
-        vertx.eventBus().registerHandler("request.createkeyspace", new CreateKeyspaceHandler());
-        vertx.eventBus().registerHandler("request.setkeyspace", new SetKeyspaceHandler());
-        vertx.eventBus().registerHandler("request.createcolumnfamily", new CreateColumnFamilyHandler());
-        vertx.eventBus().registerHandler("request.listkeyspaces", new ListKeyspacesHandler());
-        vertx.eventBus().registerHandler("request.set", new SetHandler());
-        vertx.eventBus().registerHandler("request.setcolumnfamily", new SetColumnFamilyHandler());
-        vertx.eventBus().registerHandler("request.assume", new AssumeHandler());
-        vertx.eventBus().registerHandler("request.get", new GetHandler());
-        vertx.eventBus().registerHandler("request.slice", new SliceHandler());
-        vertx.eventBus().registerHandler("request.cqlquery", new CqlQueryHandler());
-        vertx.eventBus().registerHandler("request.counter", new CounterHandler());
-        vertx.eventBus().registerHandler("request.consistency", new ConsistencyHandler());
-    }
+  public static void registerOperationHandlers(Vertx x) {
+    x.eventBus().registerHandler("request.createkeyspace", new CreateKeyspaceHandler());
+    x.eventBus().registerHandler("request.setkeyspace", new SetKeyspaceHandler());
+    x.eventBus().registerHandler("request.createcolumnfamily", new CreateColumnFamilyHandler());
+    x.eventBus().registerHandler("request.listkeyspaces", new ListKeyspacesHandler());
+    x.eventBus().registerHandler("request.set", new SetHandler());
+    x.eventBus().registerHandler("request.setcolumnfamily", new SetColumnFamilyHandler());
+    x.eventBus().registerHandler("request.assume", new AssumeHandler());
+    x.eventBus().registerHandler("request.get", new GetHandler());
+    x.eventBus().registerHandler("request.slice", new SliceHandler());
+    x.eventBus().registerHandler("request.cqlquery", new CqlQueryHandler());
+    x.eventBus().registerHandler("request.counter", new CounterHandler());
+    x.eventBus().registerHandler("request.consistency", new ConsistencyHandler());
+  }
 
 }
