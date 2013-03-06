6e7808576a2ab33c493c2490d715624e05b528de
Edward Capriolo <edlinuxguru@gmail.com>
2013-03-05 23:12:04 -0500

    Whew

--- src/main/java/org/usergrid/vx/experimental/multiprocessor/FactoryProvider.java
+++ src/main/java/org/usergrid/vx/experimental/multiprocessor/FactoryProvider.java
@@ -1,13 +1,16 @@
 package org.usergrid.vx.experimental.multiprocessor;
 
+import org.usergrid.vx.experimental.multiprocessor.groovy.GroovyClMultiProcessorFactory;
 import org.usergrid.vx.experimental.multiprocessor.groovy.GroovyMultiProcessorFactory;
 
 public class FactoryProvider {
 
   public MultiProcessorFactory getFilterFactory(String spec) {
     switch (spec) {
-      case "groovyclassloader":
+      case "groovyscript":
         return new GroovyMultiProcessorFactory();
+      case "groovyclassloader":
+        return new GroovyClMultiProcessorFactory();
       default:
         throw new IllegalArgumentException(spec + " is not yet supported for filters");
     }
