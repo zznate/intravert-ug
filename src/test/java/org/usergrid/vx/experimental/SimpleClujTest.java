3686b23bb5bc891fc34fa27fdcb113e89bcb20c2
Edward Capriolo <edlinuxguru@gmail.com>
2013-03-25 22:16:55 -0400

    New stuff

--- src/test/java/org/usergrid/vx/experimental/SimpleClujTest.java
+++ src/test/java/org/usergrid/vx/experimental/SimpleClujTest.java
@@ -2,9 +2,14 @@ package org.usergrid.vx.experimental;
 
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import junit.framework.Assert;
 
 import org.junit.Test;
 
+import clojure.lang.IPersistentMap;
 import clojure.lang.RT;
 import clojure.lang.Var;
 import clojure.lang.Compiler;
@@ -19,6 +24,38 @@ public class SimpleClujTest {
     Compiler.load(new StringReader(str));  
     Var foo = RT.var("user", "foo");
     Object result = foo.invoke("Hi", "there");
-    System.out.println(result);
+    Assert.assertEquals("Hi there", result);
   }
+  
+  @Test
+  public void bTest() throws ClassNotFoundException, IOException {
+    //RT.init();
+    RT.load("clojure/core");
+    //String str = "(ns user) (defn fil [a] ( map? a))";
+    //String str = "(ns user) (defn fil [a] (  a \"value\" ))";
+    //String str = "(ns user) (defn fil [a] (= (a \"value\") \"y\" ))";
+    //String str =   "(ns user) (defn fil [a] (if (= (a \"value\"), \"y\" ) (a) () )";
+    /*
+     * (defn abs [x]
+  (if (< x 0)
+    (- x)
+    x))
+     */
+    String str = "(ns user) (defn fil [a] (if (= (a \"value\") \"y\" ) a           )           )";
+    Compiler.load(new StringReader(str));  
+    Var foo = RT.var("user", "fil");
+    Map m = new HashMap();
+    m.put("name", "x");
+    m.put("value", "y");
+    
+    IPersistentMap m2 = clojure.lang.RT.map( m );
+    m2 = m2.assoc("name", "x");
+    m2 = m2.assoc("value", "y");
+    Object result = foo.invoke( m2 );
+    Assert.assertEquals(m, result);
+  }
+  
+  
+  
+  
 }
