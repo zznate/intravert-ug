d4d9dafc23b130916713536d6f267e08d301cec2
Edward Capriolo <edlinuxguru@gmail.com>
2013-04-11 13:24:49 -0400

    Moved assume tests paving the way for more complex assume logic

--- src/test/java/org/usergrid/vx/experimental/IntraServiceITest.java
+++ src/test/java/org/usergrid/vx/experimental/IntraServiceITest.java
@@ -119,57 +119,6 @@ public class IntraServiceITest {
     Assert.assertEquals(new Integer(1), res.getExceptionId());
   }
 
-  @Test
-  public void assumeTest() throws Exception {
-    IntraReq req = new IntraReq();
-    req.add(Operations.setKeyspaceOp("assks")); // 0
-    req.add(Operations.createKsOp("assks", 1)); // 1
-    req.add(Operations.createCfOp("asscf")); // 2
-    req.add(Operations.setColumnFamilyOp("asscf")); // 3
-    req.add(Operations.setAutotimestampOp(true)); // 4
-    req.add(Operations.assumeOp("assks", "asscf", "value", "UTF8Type"));// 5
-    req.add(Operations.setOp("rowa", "col1", "wow")); // 6
-    req.add(Operations.getOp("rowa", "col1")); // 7
-    IntraClient2 ic2 = new IntraClient2("localhost", 8080);
-    IntraRes res = ic2.sendBlocking(req);
-    List<Map> x = (List<Map>) res.getOpsRes().get(7);
-    System.out.println(res);
-    Assert.assertEquals("wow", x.get(0).get("value"));
-  }
-
-  @Test
-  public void assume2CfsTest() throws Exception {
-    IntraReq req = new IntraReq();
-    req.add(Operations.setKeyspaceOp("system"));
-    req.add(Operations.createKsOp("assks1", 1));
-    req.add(Operations.createKsOp("assks2", 1));
-
-    req.add(Operations.setKeyspaceOp("assks1"));
-    req.add(Operations.createCfOp("asscf1"));
-
-    req.add(Operations.setKeyspaceOp("assks2"));
-    req.add(Operations.createCfOp("asscf2"));
-    req.add(Operations.setAutotimestampOp(true));
-    req.add(Operations.assumeOp("assks1", "asscf1", "value", "UTF8Type"));
-    req.add(Operations.assumeOp("assks1", "asscf1", "column", "UTF8Type"));
-    req.add(Operations.assumeOp("assks2", "asscf2", "value", "Int32Type"));
-    req.add(Operations.assumeOp("assks2", "asscf2", "column", "Int32Type"));
-
-    req.add(Operations.setKeyspaceOp("assks1"));
-    req.add(Operations.setColumnFamilyOp("asscf1"));
-    req.add(Operations.setOp("rowa", "col1", "wow"));
-    req.add(Operations.getOp("rowa", "col1"));
-
-    req.add(Operations.setKeyspaceOp("assks2"));
-    req.add(Operations.setColumnFamilyOp("asscf2"));
-    req.add(Operations.setOp("rowa", 4, 3));
-    req.add(Operations.getOp("rowa", 4));
-
-    IntraClient2 ic2 = new IntraClient2("localhost", 8080);
-    IntraRes res = ic2.sendBlocking(req);
-    System.out.println(res);
-
-  }
 
   @Test
   public void processorTest() throws Exception {
