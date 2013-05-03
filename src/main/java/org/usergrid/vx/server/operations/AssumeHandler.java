40e7077c85da68bd78eb911d6223dfebd42227d4
Edward Capriolo <edlinuxguru@gmail.com>
2013-05-02 22:44:45 -0400

    Supper meta_data ranges like a-z

--- src/main/java/org/usergrid/vx/server/operations/AssumeHandler.java
+++ src/main/java/org/usergrid/vx/server/operations/AssumeHandler.java
@@ -3,6 +3,7 @@ package org.usergrid.vx.server.operations;
 import java.nio.ByteBuffer;
 
 import org.apache.cassandra.utils.ByteBufferUtil;
+import org.usergrid.vx.experimental.Operations;
 import org.vertx.java.core.eventbus.Message;
 import org.vertx.java.core.json.JsonObject;
 
@@ -27,7 +28,7 @@ public class AssumeHandler extends AbstractIntravertHandler {
     JsonObject state = event.body.getObject("state");
     JsonObject meta = state.getObject("meta");
     JsonObject metaColumn = state.getObject("metaColumn");
-    JsonObject metaRanged = state.getObject("metaranged");
+    JsonObject metaRanged = state.getObject("metaRanged");
     if (meta == null) {
       meta = new JsonObject();
     }
@@ -61,6 +62,24 @@ public class AssumeHandler extends AbstractIntravertHandler {
       metaColumn.putObject(key.toString(), new JsonObject()
               .putString("clazz", params.getString("clazz")));
       state.putObject("metaColumn", metaColumn);
+      event.reply(new JsonObject().putString(id.toString(), "OK")
+              .putObject("state", state));
+    } else if (params.getField(Operations.RANGE_START) !=null) {
+      Object start = params.getField(Operations.RANGE_START);
+      Object end = params.getField(Operations.RANGE_END);
+      ByteBuffer sbuf = HandlerUtils.byteBufferForObject(start);
+      ByteBuffer ebuf = HandlerUtils.byteBufferForObject(end);
+      StringBuilder key = new StringBuilder();
+      key.append(HandlerUtils.determineKs(params, state, null));
+      key.append(' ');
+      key.append(HandlerUtils.determineCf(params, state, null));
+      key.append(' ');
+      key.append(ByteBufferUtil.bytesToHex(sbuf));
+      JsonObject value = new JsonObject();
+      value.putString("clazz", params.getString("clazz"));
+      value.putString("end",ByteBufferUtil.bytesToHex(ebuf));
+      metaRanged.putObject(key.toString(), value);
+      state.putObject("metaRanged", metaRanged);
       event.reply(new JsonObject().putString(id.toString(), "OK")
               .putObject("state", state));
     } else {
