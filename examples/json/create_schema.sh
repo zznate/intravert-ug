dea33b89949cbb23dd5ace87e7bfe8009ab4c82b
Aaron Morton <aaron@thelastpickle.com>
2013-05-09 16:09:32 +1200

    add SETKEYSPACE so the CREATECOLUMNFAMILY works

--- examples/json/create_schema.sh
+++ examples/json/create_schema.sh
@@ -1,3 +1,3 @@
 echo "Creating keyspace 'myks' and column family 'mycf'"
-curl -vX POST http://localhost:8080/intravert/intrareq-json -d "{\"e\":[ {\"type\":\"CREATEKEYSPACE\",\"op\":{\"name\":\"myks\",\"replication\":1}}, {\"type\":\"CREATECOLUMNFAMILY\",\"op\":{\"name\":\"mycf\"}} ]}"
+curl -vX POST http://localhost:8080/intravert/intrareq-json -d "{\"e\":[ {\"type\":\"CREATEKEYSPACE\",\"op\":{\"name\":\"myks\",\"replication\":1}}, {\"type\":\"SETKEYSPACE\",\"op\":{\"keyspace\":\"myks\"}}, {\"type\":\"CREATECOLUMNFAMILY\",\"op\":{\"name\":\"mycf\"}} ]}"
 echo
