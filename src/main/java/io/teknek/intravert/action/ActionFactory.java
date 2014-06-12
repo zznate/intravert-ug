1aecc9d4ba5a7de01276deb61a6ee3de19184582
Edward Capriolo <edlinuxguru@gmail.com>
2014-06-11 22:34:22 -0400

    Create keyspace and column family for filter information

--- src/main/java/io/teknek/intravert/action/ActionFactory.java
+++ src/main/java/io/teknek/intravert/action/ActionFactory.java
@@ -3,6 +3,7 @@ package io.teknek.intravert.action;
 import java.util.HashMap;
 import java.util.Map;
 
+import io.teknek.intravert.action.impl.CreateFilterAction;
 import io.teknek.intravert.action.impl.GetKeyspaceAction;
 import io.teknek.intravert.action.impl.SaveSessionAction;
 import io.teknek.intravert.action.impl.LoadSessionAction;
@@ -14,6 +15,7 @@ public class ActionFactory {
   public static final String LOAD_SESSION = "loadsession";
   public static final String SET_KEYSPACE = "setkeyspace";
   public static final String GET_KEYSPACE = "getkeyspace";
+  public static final String CREATE_FILTER = "createfilter";
   private Map<String,Action> actions;
   
   public ActionFactory(){
@@ -22,6 +24,7 @@ public class ActionFactory {
     actions.put(LOAD_SESSION, new LoadSessionAction());
     actions.put(SET_KEYSPACE, new SetKeyspaceAction());
     actions.put(GET_KEYSPACE, new GetKeyspaceAction());
+    actions.put(CREATE_FILTER, new CreateFilterAction());
   }
   
   public Action findAction(String operation){
--- src/main/java/io/teknek/intravert/action/impl/CreateFilterAction.java
+++ src/main/java/io/teknek/intravert/action/impl/CreateFilterAction.java
@@ -1,7 +1,29 @@
 package io.teknek.intravert.action.impl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.cassandra.config.CFMetaData;
+import org.apache.cassandra.config.Schema;
+import org.apache.cassandra.db.ConsistencyLevel;
+import org.apache.cassandra.db.IMutation;
+import org.apache.cassandra.exceptions.ConfigurationException;
+import org.apache.cassandra.exceptions.InvalidRequestException;
+import org.apache.cassandra.exceptions.OverloadedException;
+import org.apache.cassandra.exceptions.UnavailableException;
+import org.apache.cassandra.exceptions.WriteTimeoutException;
+import org.apache.cassandra.service.MigrationManager;
+import org.apache.cassandra.service.StorageProxy;
+import org.apache.cassandra.thrift.CfDef;
+
+import groovy.lang.Closure;
 import io.teknek.intravert.action.Action;
 import io.teknek.intravert.action.filter.Filter;
+import io.teknek.intravert.action.filter.FilterFactory;
+import io.teknek.intravert.model.Constants;
 import io.teknek.intravert.model.Operation;
 import io.teknek.intravert.model.Response;
 import io.teknek.intravert.service.ApplicationContext;
@@ -21,13 +43,40 @@ public class CreateFilterAction implements Action {
     n.setTheClass((String) operation.getArguments().get("theClass"));
     n.setScript((String) operation.getArguments().get("script"));
     try {
-      Filter f = io.teknek.nit.NitFactory.construct(n);
-      
-      request.getSession().putFilter(name, f);
-      
-    } catch (NitException e) {
+      Filter f = FilterFactory.createFilter(io.teknek.nit.NitFactory.construct(n));
+      if ("session".equalsIgnoreCase(scope)){
+        request.getSession().putFilter(name, f);
+      } else if ("application".equalsIgnoreCase(scope)){
+        addFilterToCluster(f, n);
+        application.putFilter(name, f);
+      }
+    } catch (NitException | WriteTimeoutException | InvalidRequestException | ConfigurationException | UnavailableException | OverloadedException e) {
       throw new RuntimeException(e);
     }
+    Map m = new HashMap();
+    m.put(Constants.RESULT, Constants.OK);
+    response.getResults().put(operation.getId(), Arrays.asList(m));
   }
 
+  
+  public void maybeCreateColumnFamily() throws InvalidRequestException, ConfigurationException {
+    List<String> keyspaces = Schema.instance.getNonSystemTables();
+    if (!keyspaces.contains("intravert")){
+      CreateKeyspaceAction.createKeyspace("intravert", 1);
+      CFMetaData cfm = null;
+      CfDef def = new CfDef();
+      def.setKeyspace("intravert");
+      def.setName("filter");
+      cfm = CFMetaData.fromThrift(def);
+      cfm.addDefaultIndexNames();
+      MigrationManager.announceNewColumnFamily(cfm);
+    }
+  }
+  
+  public void addFilterToCluster(Filter f, NitDesc n) throws InvalidRequestException, ConfigurationException, WriteTimeoutException, UnavailableException, OverloadedException{
+    maybeCreateColumnFamily();
+    List<IMutation> changes = new ArrayList<>();
+    StorageProxy.mutate(changes, ConsistencyLevel.QUORUM);
+    
+  }
 }
